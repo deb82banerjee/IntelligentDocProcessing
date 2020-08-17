afb7363d30d4ae5bc534446d95042ba9e16ca1b5
Debajyoti <debajyoti@gmail.com>
2020-08-17 17:02:03 +0530

    first commit

---
+++ src/main/java/com/cts/idp/IntelligentDocProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.cts.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentDocProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentDocProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/idp/IntelligentDocProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentDocProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
