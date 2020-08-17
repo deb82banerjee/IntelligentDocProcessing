751b855b829a23dfef05ae960d49dcaa6ebbc9e9
Debajyoti <debajyoti@gmail.com>
2020-08-17 17:04:50 +0530

    Initial Commit

--- src/main/java/com/cts/idp/IntelligentDocProcessingApplication.java
+++ src/main/java/com/cts/idp/IntelligentDocProcessingApplication.java
@@ -2,12 +2,18 @@ package com.cts.idp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IntelligentDocProcessingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntelligentDocProcessingApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
